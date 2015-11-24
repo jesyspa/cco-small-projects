c474a5f975e13b32f0fbe6b7858a9f55e642ac87
Anton Golov <jesyspa@gmail.com>
2015-11-24 21:48:52 +0100

    Made tc-diag call the AG code

--- tdiagrams/src/CCO/Diag/TypeCheck.hs
+++ tdiagrams/src/CCO/Diag/TypeCheck.hs
@@ -2,8 +2,16 @@ module CCO.Diag.TypeCheck (
       tcTDiag
 ) where
 
-import CCO.Component  (Component)
-import CCO.Diag.Base  (Diag)
+import CCO.Component     (Component, component)
+import CCO.Feedback      (Message(Error), messages)
+import CCO.Printing      (text)
+import CCO.Diag.Base     (Diag)
+import CCO.Diag.AG       (wrap_Diag, err_Syn_Diag, Inh_Diag(..), sem_Diag)
+import CCO.Diag.DiagType (nullType)
 
 tcTDiag :: Component Diag Diag
-tcTDiag = undefined
+tcTDiag = component $ \diag -> do
+    let errs = maybe [] id $ err_Syn_Diag (wrap_Diag (sem_Diag diag) $ Inh_Diag nullType)
+        msgs = map (Error . text . show) errs
+    messages msgs
+    return diag
