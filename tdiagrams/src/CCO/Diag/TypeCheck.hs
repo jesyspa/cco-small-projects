c2b24559e0285644b2b9294b7ef0764c93d6dc75
Anton Golov <jesyspa@gmail.com>
2015-11-28 20:57:25 +0100

    Improved error messages

--- tdiagrams/src/CCO/Diag/TypeCheck.hs
+++ tdiagrams/src/CCO/Diag/TypeCheck.hs
@@ -2,16 +2,17 @@ module CCO.Diag.TypeCheck (
       tcTDiag
 ) where
 
-import CCO.Component     (Component, component)
-import CCO.Feedback      (Message(Error), messages)
-import CCO.Printing      (text)
-import CCO.Diag.Base     (Diag)
-import CCO.Diag.AG       (wrap_Diag, err_Syn_Diag, Inh_Diag(..), sem_Diag)
+import CCO.Component      (Component, component)
+import CCO.Feedback       (Message(Error), messages)
+import CCO.Printing       (pp)
+import CCO.Diag.TypeError (TypeError(..), TypeErrorAnn(..))
+import CCO.Diag.Base      (Diag)
+import CCO.Diag.AG        (wrap_Diag, err_Syn_Diag, Inh_Diag(..), sem_Diag)
 
 tcTDiag :: Component Diag Diag
 tcTDiag = component $ \diag -> do
   let
     errs = err_Syn_Diag $ wrap_Diag (sem_Diag diag) $ Inh_Diag
-    msgs = map (Error . text . show ) errs
+    msgs = map (Error . pp) errs
   messages msgs
   return diag
--- tdiagrams/src/CCO/Diag/TypeError.hs
+++ tdiagrams/src/CCO/Diag/TypeError.hs
@@ -1,9 +1,51 @@
 module CCO.Diag.TypeError (
       TypeError(..)
+    , TypeErrorAnn(..)
 ) where
 
 import CCO.SourcePos
+import CCO.Printing
 
-data TypeError = GenericError SourcePos String
+data TypeError = CompilerMismatch String String
+               | RuntimeMismatch String String
+               | CannotCompile
+               | CannotBeCompiled
+               | CannotRun
+               | CannotBeRun
                deriving (Eq, Read, Show)
 
+data TypeErrorAnn = TypeErrorAnn SourcePos TypeError
+                  deriving (Eq, Read, Show)
+
+-- beside with space
+infixr 3 >||<
+(>||<) :: Doc -> Doc -> Doc
+x >||< y | isEmpty x || isEmpty y = x >|< y
+         | otherwise = x >|< space >|< y
+
+ppError :: TypeError -> Doc
+ppError (CompilerMismatch x y) = text "Cannot compile a program/interpreter in" >||< text x >||< text "with a compiler for" >||< text y
+ppError (RuntimeMismatch  x y) = text "Cannot run a program/interpreter/compiler in" >||< text x >||< text "on a platform/interpreter for" >||< text y
+ppError CannotCompile = text "This block cannot be used to compile"
+ppError CannotBeCompiled = text "This block cannot be compiled"
+ppError CannotRun = text "This block cannot be used to run another block"
+ppError CannotBeRun = text "This block cannot be run"
+
+ppSource :: Source -> Doc
+ppSource (File path) = text path
+ppSource Stdin = text "<stdin>"
+
+ppPos :: Pos -> Doc
+ppPos (Pos l c) = pp l >|< colon >|< pp c
+
+ppSourcePos :: SourcePos -> Doc
+ppSourcePos (SourcePos s p) = ppSource s >|< colon >|< ppPos p
+
+ppErrorAnn :: TypeErrorAnn -> Doc
+ppErrorAnn (TypeErrorAnn pos err) = ppSourcePos pos >|< colon >||< ppError err
+
+instance Printable TypeError where
+    pp = ppError
+
+instance Printable TypeErrorAnn where
+    pp = ppErrorAnn
