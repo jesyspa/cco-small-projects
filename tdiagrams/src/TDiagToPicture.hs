587d5af2e8462e0388803c9cdb4ebf6935133f9a
Anton Golov <jesyspa@gmail.com>
2015-11-29 01:52:49 +0100

    Fixed lots of compiler errors

--- tdiagrams/src/TDiagToPicture.hs
+++ tdiagrams/src/TDiagToPicture.hs
@@ -1,10 +1,11 @@
 import CCO.Component  (Component, component, printer, ioWrap)
 import CCO.Tree       (Tree (fromTree, toTree), parser)
 import CCO.Diag       (Diag)
-import CCO.Picture    (Picture)
+import CCO.Picture    (Picture(..))
 import Control.Arrow  (Arrow (arr), (>>>))
+import CCO.Diag.AG.MakePicture
 
 tdiag2picture :: Component Diag Picture
-tdiag2picture = undefined
+tdiag2picture = arr $ \x -> Picture (200, 200) $ code_Syn_Diag $ wrap_Diag (sem_Diag x) $ Inh_Diag
 
 main = ioWrap (parser >>> component toTree >>> tdiag2picture >>> arr fromTree >>> printer)
