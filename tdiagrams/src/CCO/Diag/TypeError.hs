7e74ca669a5063ce17f4af0a9131fcb600db1a58
Giovanni Garufi <g.garufi@students.uu.nl>
2015-11-29 15:15:28 +0100

    Added some more comments

--- tdiagrams/src/CCO/Diag/TypeError.hs
+++ tdiagrams/src/CCO/Diag/TypeError.hs
@@ -7,7 +7,7 @@ import CCO.SourcePos
 import CCO.Printing
 
 data TypeError = CompilerMismatch String String
-               | RuntimeMismatch String String
+               | ExecutionMismatch String String
                | CannotCompile
                | CannotBeCompiled
                | CannotRun
@@ -25,7 +25,7 @@ x >||< y | isEmpty x || isEmpty y = x >|< y
 
 ppError :: TypeError -> Doc
 ppError (CompilerMismatch x y) = text "Cannot compile a program/interpreter in" >||< text x >||< text "with a compiler for" >||< text y
-ppError (RuntimeMismatch  x y) = text "Cannot run a program/interpreter/compiler in" >||< text x >||< text "on a platform/interpreter for" >||< text y
+ppError (ExecutionMismatch  x y) = text "Cannot run a program/interpreter/compiler in" >||< text x >||< text "on a platform/interpreter for" >||< text y
 ppError CannotCompile = text "This block cannot be used to compile"
 ppError CannotBeCompiled = text "This block cannot be compiled"
 ppError CannotRun = text "This block cannot be used to run another block"
