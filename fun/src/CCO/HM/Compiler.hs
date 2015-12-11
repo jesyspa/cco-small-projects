07ab959df4379a2b6929583b5e39ff6231eb5fcc
Anton Golov <jesyspa@gmail.com>
2015-12-11 22:40:19 +0100

    Implemented some simple translations

--- fun/src/CCO/HM/Compiler.hs
+++ fun/src/CCO/HM/Compiler.hs
@@ -9,5 +9,10 @@ import CCO.Component
 
 compile :: Component Tm Mod
 compile = component $ \tm -> do
-    let wtm = wrap_Tm (sem_Tm tm) Inh_Tm
-    return $ Mod (code_Syn_Tm wtm) (bindings_Syn_Tm wtm)
+    let inh = Inh_Tm { gsymbols_Inh_Tm = bindNames, symbols_Inh_Tm = [], inLambda_Inh_Tm = False}
+        wtm = wrap_Tm (sem_Tm tm) inh
+        binds :: [(Var, Bind)]
+        binds = bindings_Syn_Tm wtm
+        bindNames = map fst binds
+        bindVals = map snd binds
+    return $ Mod (code_Syn_Tm wtm) bindVals
