db3d202ed6d90569fa0926a6b13b2c9009bd0da1
Anton Golov <jesyspa@gmail.com>
2015-12-12 21:20:49 +0100

    Implemented multi-argument lambdas

--- fun/src/CCO/HM/ToANormal.hs
+++ fun/src/CCO/HM/ToANormal.hs
@@ -14,14 +14,14 @@ builtins = [bFalse, bTrue, bCons, bNil, bIsNil, bHead, bTail]
           bTrue  = ("True",  AAlloc 1 [])
           -- We could have done as Lisp does and made False = Nil, but the assignment requires
           -- this choice of tags, so we've got True = Nil.
-          bCons  = ("Cons",  ALam "$b_head" $ AExp $ ALam "$b_tail" $ AExp $ AAlloc 0 [AVar "$b_head", AVar "$b_tail"])
+          bCons  = ("Cons",  ALam ["$b_head", "$b_tail"] $ AExp $ AAlloc 0 [AVar "$b_head", AVar "$b_tail"])
           bNil   = ("Nil",   AVal $ AVar "True")
           -- Thanks to the way we represent Cons and Nil, isNil can be the identity function.
           -- This is, of course, rather questionable from the point of view of garbage collection
           -- and efficient memory usage.
-          bIsNil = ("isNil", ALam "$b_list" $ AExp $ AVal $ AVar "$b_list")
-          bHead  = ("head",  ALam "$b_list" $ AExp $ AVal $ AField "$b_list" 0)
-          bTail  = ("tail",  ALam "$b_list" $ AExp $ AVal $ AField "$b_list" 1)
+          bIsNil = ("isNil", ALam ["$b_list"] $ AExp $ AVal $ AVar "$b_list")
+          bHead  = ("head",  ALam ["$b_list"] $ AExp $ AVal $ AField "$b_list" 0)
+          bTail  = ("tail",  ALam ["$b_list"] $ AExp $ AVal $ AField "$b_list" 1)
 
 toANormal :: Component Tm ATm
 toANormal = component $ \tm -> do
