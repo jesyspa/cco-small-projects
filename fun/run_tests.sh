abbd550ec427de931dab9ac0a7ebda12f2cca8cc
Anton Golov <jesyspa@gmail.com>
2015-12-13 16:55:02 +0100

    Added lazy-example again

--- fun/run_tests.sh
+++ fun/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function run_test {
+function run_test_impl {
     a=$1
     rm -f $a.hm-aterm $a.core-aterm $a.tcrr $a.out
     cat $a.lam        | dist/build/parse-hm/parse-hm > $a.hm-aterm
@@ -15,19 +15,20 @@ function run_test {
     return $?
 }
 
-if [[ $1 ]]; then
-    run_test $1
+function run_test {
+    echo Running $1...
+    run_test_impl $1
     rc=$?
     if [[ $rc != 0 ]]; then
-        echo "Test failed:" $1 "with code" $rc
+        echo Test failed: $1 with code $rc
     fi
+}
+
+if [[ $1 ]]; then
+    run_test $1
 else
     for tfile in tests/*.lam; do
         run_test ${tfile%.lam}
-        rc=$?
-        if [[ $rc != 0 ]]; then
-            echo "Test failed:" $tfile "with code" $rc
-        fi
     done
 fi
 
