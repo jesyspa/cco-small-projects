ec7bc59fd5876bf32758b722c3e8176b5a224445
Anton Golov <jesyspa@gmail.com>
2015-12-20 00:26:43 +0100

    Renamed hmcr to hm2cr
    As per spec

--- fun/run_tests.sh
+++ fun/run_tests.sh
@@ -5,7 +5,7 @@ function run_test_impl {
     rm -f $a.hm-aterm $a.core-aterm $a.tcrr $a.out
     cat $a.lam        | dist/build/parse-hm/parse-hm > $a.hm-aterm
     if [[ $? != 0 ]]; then return 10; fi
-    cat $a.hm-aterm   | dist/build/hmcr/hmcr         > $a.core-aterm
+    cat $a.hm-aterm   | dist/build/hm2cr/hm2cr       > $a.core-aterm
     if [[ $? != 0 ]]; then return 11; fi
     cat $a.core-aterm | dist/build/pp-core/pp-core   > $a.tcrr
     if [[ $? != 0 ]]; then return 12; fi
