698fd8286564996bc0d7e260a032cece432f2cfb
Giovanni Garufi <g.garufi@students.uu.nl>
2015-12-20 13:36:12 +0100

    run_tests now uses uhcr from sandbox

--- fun/run_tests.sh
+++ fun/run_tests.sh
@@ -5,11 +5,11 @@ function run_test_impl {
     rm -f $a.hm-aterm $a.core-aterm $a.tcrr $a.out
     cat $a.lam        | dist/build/parse-hm/parse-hm > $a.hm-aterm
     if [[ $? != 0 ]]; then return 10; fi
-    cat $a.hm-aterm   | dist/build/hm2cr/hm2cr       > $a.core-aterm
+    cat $a.hm-aterm   | dist/build/hm2cr/hm2cr         > $a.core-aterm
     if [[ $? != 0 ]]; then return 11; fi
     cat $a.core-aterm | dist/build/pp-core/pp-core   > $a.tcrr
     if [[ $? != 0 ]]; then return 12; fi
-    uhcr --corerunopt=printresult $a.tcrr > $a.out
+    .cabal-sandbox/bin/uhcr --corerunopt=printresult $a.tcrr > $a.out
     if [[ $? != 0 ]]; then return 13; fi
     diff $a.out $a.exp
     return $?
@@ -31,4 +31,3 @@ else
         run_test ${tfile%.lam}
     done
 fi
-
