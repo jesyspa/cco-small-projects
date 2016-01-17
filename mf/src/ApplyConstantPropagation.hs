8e8ed7183f1b198f08dcb3261ee4513326615056
Anton Golov <jesyspa@gmail.com>
2016-01-17 01:49:18 +0100

    Generalised application of constant propagation

--- mf/src/ApplyConstantPropagation.hs
+++ mf/src/ApplyConstantPropagation.hs
@@ -7,5 +7,5 @@ import AG.ApplyConstantPropagation
 import Analysis
 import qualified Data.Map as M
 
-propagateConstants :: AnalysisResult (M.Map String Int) -> Stat' -> Stat'
-propagateConstants result stat = code_Syn_Stat' $ wrap_Stat' (sem_Stat' stat) (Inh_Stat' result)
+propagateConstants :: AnalysisResult (M.Map String Int) -> Program' -> Program'
+propagateConstants result prog = sem_Program' prog result
